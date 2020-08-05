1425b79b65628bf712af913fa8e638ce777de328
Liam DiGregorio <11711732+devli13@users.noreply.github.com>
2020-08-05 14:52:23 -0700

    Update wow.sol

--- contracts/wow.sol
+++ contracts/wow.sol
@@ -2,10 +2,10 @@
 
 pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
+import "openzeppelin/contracts/access/Ownable.sol";
+import "openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20Pausable.sol";
